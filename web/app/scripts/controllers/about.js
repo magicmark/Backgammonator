ad7d97039243cd16d00b0660609bf9a0b5e2752a
Mark <mark@larah.me>
2015-03-06 14:20:49 +0000

    first commit

---
+++ web/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name backgammonatorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the backgammonatorApp
+ */
+angular.module('backgammonatorApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name backgammonatorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the backgammonatorApp
+ */
+angular.module('backgammonatorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
